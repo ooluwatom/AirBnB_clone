695e1c974dd5a6e81d6afac749cc536d3c77dc40
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-01-16 21:46:34 +0100

    BaseModel class created

--- models/base_model.py
+++ models/base_model.py
@@ -6,9 +6,11 @@ from datetime import datetime
 
 class BaseModel:
     '''The Base Class'''
-    id = f'{uuid.uuid4()}'
-    created_at = datetime.now()
-    updated_at = datetime.now()
+    def __init__(self):
+        '''Base model __init__ method'''
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         '''Print instance details'''
@@ -16,14 +18,11 @@ class BaseModel:
 
     def save(self):
         '''Updates the time of update'''
-        updated_at = datetime.now()
-        return updated_at
+        self.updated_at = datetime.now()
+        return self.updated_at
 
     def to_dict(self):
         '''Returns dictionary containing all keys/values of __dict__'''
         obj_dict = self.__dict__
         obj_dict['__class__'] = self.__class__.__name__
-        self.id = self.id
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
         return obj_dict
