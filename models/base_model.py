bef2be29219face97498223f9a0d6f8c2604b226
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-01-16 22:45:39 +0100

    Update models/base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,22 @@ from datetime import datetime
 
 class BaseModel:
     '''The Base Class'''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''Base model __init__ method'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, val in kwargs.items():
+                if key in ('created_at', 'updated-at'):
+                    val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+
+                if key != '__class__':
+                    setattr(self, key, val)
+                else:
+                    continue
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         '''Print instance details'''
