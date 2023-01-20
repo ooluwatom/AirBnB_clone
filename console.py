0e54b3da13eea42e0b8a16893904cff66b16fb50
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-01-20 20:00:16 +0100

    commit

---
+++ console.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    '''The HBNB command'''
+
+    def do_quit(self, line):
+        '''Quit the program'''
+        return True
+
+    def do_EOF(self, arg):
+        '''Exit the command interpreter'''
+        return True
+
+    def do_help(self, arg):
+        '''Prints all available commands'''
+        cmd.Cmd.do_help(self, arg)
+
+    def do_hbnb(self, arg):
+        """
+        Prints "Welcome to HBNB", a custom hbnb command
+        """
+        print("Welcome to HBNB")
+
+    def help_hbnb(self):
+        """
+        Prints the docstring of the hbnb command
+        """
+        print(self.do_hbnb.__doc__)
+
+    def emptyline(self):
+        '''Returns nothing when there is an empty line'''
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
