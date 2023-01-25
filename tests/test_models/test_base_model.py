0ed55350225794eec33bb2d2ed4c80f59b159b9f
Powerisinschool <90867477+ooluwatom@users.noreply.github.com>
2023-01-25 22:37:12 +0100

    adding unittests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Unittest for base model module.
 
-import os
-import unittest
-import uuid
+This unittest is a collection of possible edge cases
+on which this module should not be expected to fail,
+and cases on which it is expected to fail.
+
+"""
+
 from datetime import datetime
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.state import State
+from models.review import Review
+import os
+import pep8
+import unittest
+import uuid
 
 
 class TestBaseModel(unittest.TestCase):
+    """this will test the base model class"""
     @classmethod
     def setUpClass(cls):
         """setup for the test"""
@@ -27,11 +42,26 @@ class TestBaseModel(unittest.TestCase):
         except Exception:
             pass
 
+    def test_pep8_conformance_base_model(self):
+        """pep8 test.
+
+        This test is designed to make sure the Python code
+        is up to the pep8 standard.
+
+        """
+        syntax = pep8.StyleGuide(quit=True)
+        check = syntax.check_files(['models/base_model.py'])
+        self.assertEqual(
+            check.total_errors, 0,
+            "Found code style errors (and warnings)."
+        )
 
     def test_base_model_id_is_string(self):
         """UUID format testing.
+
         This test is designed to check if any generated UUID
         is correctly generated and has the propper format.
+
         """
         bm = BaseModel()
         self.assertIsInstance(bm.id, str)
@@ -78,16 +108,20 @@ class TestBaseModel(unittest.TestCase):
 
     def test_base_model_created_at_is_datetime(self):
         """Datetime test.
+
         This test is designed to check if the date and time in which a
         class was created are correctly assigned.
+
         """
         bm = BaseModel()
         self.assertIsInstance(bm.created_at, datetime)
 
     def test_base_model_updated_at_is_datetime(self):
         """Datetime test.
+
         This test is designed to check if the date and time in which a
         class is updated are correctly assigned.
+
         """
         bm = BaseModel()
         self.assertIsInstance(bm.updated_at, datetime)
